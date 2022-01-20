cca38e37f03caa70ae01a6172ccc861ce29c5246
Tan Yu Yang <tyy356@gmail.com>
2022-01-20 13:52:41 +0800

    Update indentation

--- backend/main.py
+++ backend/main.py
@@ -5,22 +5,28 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app = Flask(__name__)
 api = Api(app)
-#read the database here INCLUDE THIS 
+# read the database here INCLUDE THIS
+
 
 class PostsModel(db.Model):
 	Post_ID = db.Column(db.Integer, primary_key=True)
 	Post_Title = db.Column(db.String(100))
 	Post_Description = db.Column(db.String(100))
-	Post_Image = db.Column(db.String(100)) #
+	Post_Image = db.Column(db.String(100))
 
 	def __repr__(self):
 		return f"posts(Post_ID = {Post_ID}, Post_Title = {Post_Title}, Post_Description = {Post_Description}, Post_Image = {Post_Image})"
 
+
 posts_put_args = reqparse.RequestParser()
-posts_put_args.add_argument("Post_ID", type=int, help="Post_ID is required", required=True)
-posts_put_args.add_argument("Post_Title", type=str, help="Post_Title is required", required=True)
-posts_put_args.add_argument("Post_Description", type=str, help="Post_Description is required", required=True)
-posts_put_args.add_argument("Post_Image", type=str, help="Post_Image is required", required=True)
+posts_put_args.add_argument(
+    "Post_ID", type=int, help="Post_ID is required", required=True)
+posts_put_args.add_argument("Post_Title", type=str,
+                            help="Post_Title is required", required=True)
+posts_put_args.add_argument(
+    "Post_Description", type=str, help="Post_Description is required", required=True)
+posts_put_args.add_argument("Post_Image", type=str,
+                            help="Post_Image is required", required=True)
 
 resource_fields = {
 	'Post_ID': fields.Integer,
@@ -29,13 +35,14 @@ resource_fields = {
 	'Post_Image': fields.String
 }
 
+
 class Posts(Resource):
 	@marshal_with(resource_fields)
 	def get(self):
 		result = PostsModel.query.all()
 		return result
 
-    @marshal_with(resource_fields)
+	@marshal_with(resource_fields)
 	def put(self, post_id):
 		args = posts_put_args.parse_args()
 		result = PostsModel.query.filter_by(Post_ID=post_id).first()
