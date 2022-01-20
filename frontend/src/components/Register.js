6c5115b2367a58a8a9a2ff1ebe4802836c717dcb
Tan Yu Yang <tyy356@gmail.com>
2022-01-20 12:27:27 +0800

    Add Register UI

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,8 +1,124 @@
+// Register Page
+
+import React, { useState } from "react";
+import "../styles/Register.css";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+function validatePassword(password, confirmPassword) {
+  if (password !== confirmPassword){
+    return false
+  }
+}
+
+function handleSubmit(){
+  
+}
 
 const Register = () => {
-  return <div>
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
 
-  </div>;
+  return (
+    <div className='Register'>
+      <h1>Register</h1>
+      <Form>
+        <Form.Group controlId="username">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            autoFocus
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="confirmPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="age">
+          <Form.Label>Age</Form.Label>
+          <Form.Control value={age} onChange={(e) => setAge(e.target.value)} />
+        </Form.Group>
+
+        <Form.Group controlId="birthday">
+          <Form.Label>Birthday</Form.Label>
+          <Form.Control
+            type="date"
+            value={birthday}
+            onChange={(e) => setBirthday(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="name@example.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="phone">
+          <Form.Label>Phone</Form.Label>
+          <Form.Control
+            type="tel"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="city">
+          <Form.Label>City</Form.Label>
+          <Form.Control
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="country">
+          <Form.Label>Country</Form.Label>
+          <Form.Control
+            value={country}
+            onChange={(e) => setCountry(e.target.value)}
+          />
+        </Form.Group>
+
+        <Button variant='primary' onClick={() => handleSubmit} type="submit">Submit</Button>
+      </Form>
+    </div>
+  );
 };
 
 export default Register;
