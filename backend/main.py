1c329fe261a3b289f62391b2dff10f550fc376f6
Josiah Goh <59958371+JoGoh@users.noreply.github.com>
2022-01-20 12:23:49 +0800

    fix: add password in class usermodel

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,7 @@ db = SQLAlchemy(app) #wrap app
 
 class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True) #primary key true -> this field has to be unique
+    password = db.Column(db.String(100), nullable=False)
     name = db.Column(db.String(100), nullable=False) #nullable false -> this field has to have info
     age = db.Column(db.Integer, nullable=False)
     birthday = db.Column(db.String(100), nullable=False)
