c51e3553405b1982fbaa5a8504ab0d8bd1c58f10
Nicholas <nickchua97@gmail.com>
2022-01-20 12:55:57 +0800

    Login Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Login from "./components/login/login";
 
 function App() {
