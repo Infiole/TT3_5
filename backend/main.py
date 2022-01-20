16121cfbf6d284d519e6dadb98f3a4d57bd1fd95
lindafrancis3 <lindafrancis3@hotmail.com>
2022-01-20 13:24:33 +0800

    return all posts and insert post

--- backend/main.py
+++ backend/main.py
@@ -12,21 +12,48 @@ db = SQLAlchemy(app)  # wrap app
 
 
 class UserModel(db.Model):
-    # primary key true -> this field has to be unique
-    USER_ID = db.Column(db.Integer, primary_key=True)
-    #password = db.Column(db.String(100), nullable=False)
-    # nullable false -> this field has to have info
-    Name = db.Column(db.String(100), nullable=False)
-    Age = db.Column(db.Integer, nullable=False)
-    Birthday = db.Column(db.String(100), nullable=False)
-    Email = db.Column(db.String(100), nullable=False)
-    Phone = db.Column(db.Integer, nullable=False)
-    City = db.Column(db.String(100), nullable=False)
-    Country = db.Column(db.String(100), nullable=False)
+	# primary key true -> this field has to be unique
+	USER_ID = db.Column(db.Integer, primary_key=True)
+	# password = db.Column(db.String(100), nullable=False)
+	# nullable false -> this field has to have info
+	Name = db.Column(db.String(100), nullable=False)
+	Age = db.Column(db.Integer, nullable=False)
+	Birthday = db.Column(db.String(100), nullable=False)
+	Email = db.Column(db.String(100), nullable=False)
+	Phone = db.Column(db.Integer, nullable=False)
+	City = db.Column(db.String(100), nullable=False)
+	Country = db.Column(db.String(100), nullable=False)
 
-    def __repr__(self):  # wrapper method, so when print this out, get valid json format
-        return f"User(name = {name}, age = {age}, birthday = {birthday}, email = {email}, phone = {phone}, city = {city}, country = {country})"
+	def __repr__(self):  # wrapper method, so when print this out, get valid json format
+		return f"User(name = {name}, age = {age}, birthday = {birthday}, email = {email}, phone = {phone}, city = {city}, country = {country})"
 
+
+class PostsModel(db.Model):
+	Post_ID = db.Column(db.Integer, primary_key=True)
+	Post_Title = db.Column(db.String(100))
+	Post_Description = db.Column(db.String(100))
+	Post_Image = db.Column(db.String(100))
+
+	def __repr__(self):
+		return f"posts(Post_ID = {Post_ID}, Post_Title = {Post_Title}, Post_Description = {Post_Description}, Post_Image = {Post_Image})"
+
+
+posts_put_args = reqparse.RequestParser()
+posts_put_args.add_argument(
+	"Post_ID", type=int, help="Post_ID is required", required=True)
+posts_put_args.add_argument("Post_Title", type=str,
+							help="Post_Title is required", required=True)
+posts_put_args.add_argument(
+	"Post_Description", type=str, help="Post_Description is required", required=True)
+posts_put_args.add_argument("Post_Image", type=str,
+							help="Post_Image is required", required=True)
+
+resource_fields = {
+	'Post_ID': fields.Integer,
+	'Post_Title': fields.String,
+	'Post_Description': fields.String,
+	'Post_Image': fields.String
+}
 
 # class PostModel(db.Model):
 #     Post_ID = db.Column(db.Integer, primary_key=True)
@@ -42,7 +69,7 @@ class UserModel(db.Model):
 # ONLY once
 #############
 db.create_all()  # only call once to instantiate, if keep calling, overrides what we alr have
-#db.session.commit()
+# db.session.commit()
 
 
 # users = [
@@ -157,16 +184,35 @@ db.create_all()  # only call once to instantiate, if keep calling, overrides wha
 #     db.session.commit()
 
 
+class Posts(Resource):
+	@marshal_with(resource_fields)
+	def get(self):
+		result = PostsModel.query.all()
+		return result
+
+	@marshal_with(resource_fields)
+	def put(self, post_id):
+		args = posts_put_args.parse_args()
+		result = PostsModel.query.filter_by(Post_ID=post_id).first()
+		if result:
+			abort(409, message="Post id taken (must be unique)")
+
+		post = PostsModel(Post_ID = post_id, post = args['Post_Title'], Post_Description = args['Post_Description'], Post_Image = args['Post_Image'])
+		db.session.add(post)
+		db.session.commit()
+		return post, 201
+
+api.add_resource(Posts, "/posts")
 
 @app.route('/')
 def home():
-    return {"message": "test TT3_5"}
+	return {"message": "test TT3_5"}
 
 
 @app.route('/home', methods=['GET'])
 def testhome():
 
-    return {"message": "hello TT3_5"}
+	return {"message": "hello TT3_5"}
 
 
 app.register_blueprint(login_api)
