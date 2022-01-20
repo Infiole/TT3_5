e777315fbeff50514338576b6dcb85abe9514d69
Nicholas <nickchua97@gmail.com>
2022-01-20 13:47:22 +0800

    usercomment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Login from "./components/login/login";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Login from "./components/login/login";
+import Comment from "./components/feature/comment";
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      <Comment />
     </div>
   );
 }
---
+++ frontend/src/components/feature/comment.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Comment() {
+  return (
+    <div className="user-with-comments">
+      <h3>Nicholas</h3>
+      <span className="comments">comments</span>
+    </div>
+  );
+}
+
+export default Comment;
